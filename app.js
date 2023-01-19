89328a19817598d669c9028f5e0c535a7919ff91
collins <collinsolayemi@gmail.com>
2023-01-19 02:52:56 -0800

    more changes

--- app.js
+++ app.js
@@ -3,20 +3,14 @@ require("dotenv").config();
 require("express-async-errors");
 const app = express();
 
-//connect DB
 const connectDB = require("./db/connect");
 
-// error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-// extra packages
-
-//importing the router
 const authRoutes = require("./routes/auth");
 const jobRoutes = require("./routes/jobs");
 
-//middleware
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/jobs", jobRoutes);
@@ -24,7 +18,6 @@ app.use("/api/v1/jobs", jobRoutes);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-//starting the server
 const port = process.env.PORT || 3008;
 const start = async () => {
   try {
